0403f2d14a648badbf9a04fb81ef2284d408a1a3
Dicenicepl <dicenicepl@gmail.com>
2026-01-09 20:14:40 +0100

    Database added

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template, redirect
+from flask import Flask
+from database import db
 from controllers.links_controller import links_bp
 
 app = Flask(__name__)
+
+#Registering outsite controllers
 app.register_blueprint(links_bp)
+
+#URI where to read/create .db file
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+
+db.init_app(app)
+
+#Create all imported tables
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
