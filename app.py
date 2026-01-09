888a49752cda106d80fc34be631c5535ff37c772
Dicenicepl <dicenicepl@gmail.com>
2026-01-09 19:59:45 +0100

    Database

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, redirect
+from flask import Flask
+from database import db
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
