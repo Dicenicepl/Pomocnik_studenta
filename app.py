8b6575f3d7938ca7eadc191aa53ff4cff5b6a926
patrosik7-dev <patrosik7@gmail.com>
2026-01-13 19:19:55 +0100

    Notes HTML commit

--- app.py
+++ app.py
@@ -1,10 +1,73 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+=======
+from flask import Flask, render_template
+from database import db
+from notes_controller import notes_bp
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notes.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+app.register_blueprint(notes_bp)
+
+with app.app_context():
+    db.create_all()
+
+>>>>>>> Stashed changes
 @app.route("/")
 def index():
     return render_template("index.html")
 
+<<<<<<< Updated upstream
 if __name__ == '__main__':
     app.run(debug=True)
+=======
+@app.route("/tasks")
+def tasks():
+    return render_template("Tasks.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("Calendar.html")
+
+@app.route("/notes")
+def notes():
+    return render_template("Notes.html")
+
+@app.route("/pomodoro")
+def pomodoro():
+    return render_template("Pomodoro.html")
+
+@app.route("/reminders")
+def reminders():
+    return render_template("Reminders.html")
+
+@app.route("/notifications")
+def notifications():
+    return render_template("Notifications.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("Settings.html")
+
+@app.route("/fast-urls")
+def fast_urls():
+    return render_template("Fast_URLs.html")
+
+@app.route("/grades")
+def grades():
+    return render_template("Grades_ECTS_points.html")
+
+@app.route("/backup")
+def backup():
+    return render_template("Backup_data_export.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+>>>>>>> Stashed changes
