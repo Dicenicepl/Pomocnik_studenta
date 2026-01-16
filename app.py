6cc9ddabd4d2e269764258b008b286b09480f1a0
Dicenicepl <dicenicepl@gmail.com>
2026-01-16 01:25:07 +0100

    prep

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from database import db
-from models.note import link
+from models.note import Note
+from models.calendar import Calendar
+from models.link import Link
+
 
 from controllers.links_controller import links_bp
 
 app = Flask(__name__)
+
+@app.get("/")
+def dashboard():
+    return render_template("Dashboard.html")
 
 #URI where to read/create .db file
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -20,4 +27,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="192.168.1.189")
